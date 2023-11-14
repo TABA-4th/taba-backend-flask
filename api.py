ff363a940b2c6e805bfb1ad5a9fdc85220bd3f3d
0-zoo <dldl0010987@dankook.ac.kr>
2023-11-14 10:53:48 +0900

    conf: flask 환경 세팅

---
+++ api.py
@@ -0,0 +1,17 @@
+# api.py
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # 포트 변경
+    # app.run(debug=True, port=8080)
