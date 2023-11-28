db5613099ebe4b1a6119e17273d69c1a76e4c342
0-zoo <dldl0010987@gmail.com>
2023-11-28 15:42:35 +0900

    feat: 평균 대비 사용자 점수 반환 기능

--- app.py
+++ app.py
@@ -7,19 +7,26 @@ from shared_data import Instance
 from _1_upload import upload
 from _2_predict import predict
 from _3_db_save import db_save
+from _4_average import average
 from config import AWS_S3_BUCKET_NAME, AWS_S3_BUCKET_REGION, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY
 from flask_restx import Api, Resource, reqparse
 
-# 전역변수 값
-Instance.now = datetime.now()
-Instance.member_id = 4  # 임시로 4로 설정
+# 전역변수 값 설정 
+Instance.member_id = 4          # 사용자 아이디, 임시값 4
+Instance.member_age = 20        # 사용자 나이, 임시값 20
+Instance.member_gender = '남자'  # 사용자 성별, 임시값 남자
+Instance.member_percentage = 0  # 사용자 평균 대비 퍼센트
+
+Instance.file_data = ''         # 사용자가 업로드한 이미지 데이터
+Instance.image_url = ''         # S3에 저장한 이미지 URL
+
+Instance.now = datetime.now()   # 현재 시간
 Instance.url_time = re.sub(r"[^0-9]", "", str(Instance.now))
 Instance.db_time = Instance.now.strftime('%Y-%m-%d %H:%M:%S')
-Instance.model_path = 'aram_fine_crust231109.pt'
-Instance.class_names = [0, 1, 2, 3]
-Instance.result = [-1, -1, -1, -1, -1, -1]
-Instance.image_url = ''
-Instance.file_data = ''
+
+Instance.model_path = 'aram_fine_crust231109.pt'    # 모델 경로
+Instance.class_names = [0, 1, 2, 3]                 # 예측 클래스 이름(0,1,2,3)
+Instance.result = [-1, -1, -1, -1, -1, -1]          # 예측 결과
 
 app = Flask(__name__)
 api = Api(app, version='0.1', title='타바 프로젝트 - AI 이미지 분석 개발서버 Flask API',
@@ -50,9 +57,11 @@ class analysis(Resource):
 
         # DB에 결과 데이터 저장
         db_save()
+
+        # 동성, 동나이대 평균 반환
+        average()
         
         return jsonify({'class': Instance.result, 'url': Instance.image_url, 'msg': 'Data saved to database successfully'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
