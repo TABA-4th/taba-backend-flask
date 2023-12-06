651ae8c017e061dfe8fc8fcd624f274be8373dd4
kolo <kolo@kakao.com>
2023-12-07 03:33:12 +0900

    refactor: change transform logics & processing 1.5 value

--- _2_predict.py
+++ _2_predict.py
@@ -6,9 +6,14 @@ from shared_data import Instance
 
 def predict(idx):
     transform = transforms.Compose([
-        transforms.Resize([224, 224]),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(20),
+    transforms.RandomAffine(0, shear=15, scale=(0.8, 1.2)),
+    transforms.Resize([int(224), int(224)], interpolation=4),
+    transforms.ToTensor(),
+    transforms.Lambda(random_transforms),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     # 파일 데이터를 메모리에 로드
@@ -29,4 +34,9 @@ def predict(idx):
     with torch.no_grad():
         outputs = model(input_tensor)
         _, predicted = torch.max(outputs, 1)
-        Instance.result[idx] = predicted[0].item()
+
+        # 특정 모델에 대한 조건적 처리
+        if idx in [0, 1] and predicted[0].item() in [1, 2]:
+            Instance.result[idx] = 1.5
+        else:
+            Instance.result[idx] = predicted[0].item()
